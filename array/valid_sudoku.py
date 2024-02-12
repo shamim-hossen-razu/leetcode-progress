8d9c2144c9776e3145f0f074d68fa1b26d8d466c
shamim.hossen <shamim.hossen@bjitgroup.com>
2024-02-12 17:22:54 +0600

    valid sudoku

---
+++ array/valid_sudoku.py
@@ -0,0 +1,67 @@
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        sudoku = []
+        for row in board:
+            rows = []
+            for col in row:
+                if col == '.':
+                    rows.append(0)
+                else:
+                    rows.append(int(col))
+            sudoku.append(rows)
+
+        
+        # check rows            
+        for row in sudoku:
+            check = []
+            for value in row:
+                if value != 0 and value in check:
+                    return False
+                else:
+                    check.append(value)
+                    
+        # check cols
+        for col in range (0,9):
+            check = []
+            for row in range(0, 9):
+                if sudoku[row][col] != 0 and sudoku[row][col] in check:
+                    return False
+                else:
+                    check.append(sudoku[row][col])
+                    
+        return True
+    
+    
+        
+            
+                
+                
+        
+        
+        
+    
+            
+        
+            
+            
+        
+                    
+                    
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        
+        
+print(Solution().isValidSudoku([["5","3",".",".","7",".",".",".","."]
+                                ,["6",".",".","1","9","5",".",".","."]
+                                ,[".","9","8",".",".",".",".","6","."]
+                                ,["8",".",".",".","6",".",".",".","3"]
+                                ,["4",".",".","8",".","3",".",".","1"]
+                                ,["7",".",".",".","2",".",".",".","6"]
+                                ,[".","6",".",".",".",".","2","8","."]
+                                ,[".",".",".","4","1","9",".",".","5"]
+                                ,[".",".",".",".","8",".",".","7","9"]]))
+
+        
